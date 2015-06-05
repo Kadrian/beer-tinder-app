8f9e83a807abfe879e90b0a2e7450ec7daadb65b
Kai Rollmann <kai.rollmann@googlemail.com>
2015-06-05 15:38:57 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".buddy").on("swiperight",function(){
+
+      $(this).addClass('rotate-left').delay(700).fadeOut(1);
+      $('.buddy').find('.status').remove();
+      $(this).append('<div class="status like">PROST!</div>');
+
+      if ( $(this).is(':first-child') ) {
+        $('.buddy:last-child').removeClass ('rotate-left rotate-right').fadeIn(300);
+        console.log("yo-right");
+      } else {
+        $(this).prev().removeClass('rotate-left rotate-right').fadeIn(400);
+      }
+    });
+
+   $(".buddy").on("swipeleft",function(){
+    $(this).addClass('rotate-right').delay(700).fadeOut(1);
+    $('.buddy').find('.status').remove();
+    $(this).append('<div class="status dislike">WUARG!</div>');
+
+    if ( $(this).is(':first-child') ) {
+      $('.buddy:last-child').removeClass ('rotate-left rotate-right').fadeIn(300);
+      console.log("yo-left");
+    } else {
+      $(this).prev().removeClass('rotate-left rotate-right').fadeIn(400);
+    }
+  });
+});
