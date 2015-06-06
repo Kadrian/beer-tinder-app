18855c07b35dbef1aaf9a93cd85edd13396dd0b6
Kai Rollmann <kai.rollmann@googlemail.com>
2015-06-06 09:48:46 +0200

    Rename buddy --> beer

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    $(".buddy").on("swiperight",function(){
+    $(".beer").on("swiperight",function(){
 
       $(this).addClass('rotate-left').delay(700).fadeOut(1);
-      $('.buddy').find('.status').remove();
+      $('.beer').find('.status').remove();
       $(this).append('<div class="status like">PROST!</div>');
 
       if ( $(this).is(':first-child') ) {
-        $('.buddy:last-child').removeClass ('rotate-left rotate-right').fadeIn(300);
+        $('.beer:last-child').removeClass ('rotate-left rotate-right').fadeIn(300);
         console.log("yo-right");
       } else {
         $(this).prev().removeClass('rotate-left rotate-right').fadeIn(400);
@@ -14,13 +14,13 @@ $(document).ready(function(){
       // Add post request to /statistics here to update like count in db
     });
 
-   $(".buddy").on("swipeleft",function(){
+   $(".beer").on("swipeleft",function(){
     $(this).addClass('rotate-right').delay(700).fadeOut(1);
-    $('.buddy').find('.status').remove();
+    $('.beer').find('.status').remove();
     $(this).append('<div class="status dislike">WUARG!</div>');
 
     if ( $(this).is(':first-child') ) {
-      $('.buddy:last-child').removeClass ('rotate-left rotate-right').fadeIn(300);
+      $('.beer:last-child').removeClass ('rotate-left rotate-right').fadeIn(300);
       console.log("yo-left");
     } else {
       $(this).prev().removeClass('rotate-left rotate-right').fadeIn(400);
